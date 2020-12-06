6b04bde8b4efaf49cb1aea174e28e2439fdaf68c
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2020-12-06 15:03:14 +0530

    Added a feature to quit out of the calculator

--- Calculator.java
+++ Calculator.java
@@ -22,36 +22,43 @@ public class Calculator {
 
 		Scanner sc = new Scanner(System.in);
 
-		int num1, num2;
+		int num1 = 0, num2 = 0;
 		int choice;
 
 		System.out.println("THIS IS A SIMPLE CALCULATOR");
-		System.out.print("Press '1' to add two numbers\nPress '2' to subtract two numbers\nPress '3' to multiply two numbers\nPress '4' to divide two numbers\n\nYour choice: ");
+		System.out.print("Press '1' to add two numbers\nPress '2' to subtract two numbers\nPress '3' to multiply two numbers\nPress '4' to divide two numbers\nEnter '10100' without quotes to quit\n\nYour choice: ");
 
 		choice = sc.nextInt();
 
-		while(choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+		while(choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 10100) {
 
-			System.out.print("Invalid choice, try again\nYour choice: ");
+			System.out.print("Invalid choice, try again or enter '10100' to quit (without quotes)\nYour choice: ");
 			choice = sc.nextInt();
 		}
 
+		
+		if (choice != 10100) {
 
-		System.out.print("Enter the numbers: ");
-		num1 = sc.nextInt();
-		num2 = sc.nextInt();
+			System.out.print("Enter the numbers: ");
+			num1 = sc.nextInt();
+			num2 = sc.nextInt();
+
+		}
 
 		if (choice == 1)
-			System.out.println("Sum is: " + add(num1, num2));
+			System.out.println("Sum is: " + add(num1, num2) + "\nThank you for using this calculator");
 
 		else if (choice == 2)
-			System.out.println("Difference is: " + subtract(num1, num2));
+			System.out.println("Difference is: " + subtract(num1, num2) + "\nThank you for using this calculator");
 
 		else if (choice == 3)
-			System.out.println("Product is: " + multiply(num1, num2));
+			System.out.println("Product is: " + multiply(num1, num2) + "\nThank you for using this calculator");
 
 		else if (choice == 4)
-			System.out.println("Result of division is: " + divide(num1, num2));
+			System.out.println("Result of division is: " + divide(num1, num2) + "\nThank you for using this calculator");
+
+		else if (choice == 10100)
+			System.out.println("\n\n...Quitting...\nThank you for using this calculator");
 
 		else
 			System.out.println("Invalid Input");
