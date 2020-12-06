a167d789a2e12d377d1f8072103f0aee19b90fce
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2020-12-06 14:41:40 +0530

    Added a correction loop for choice

--- Calculator.java
+++ Calculator.java
@@ -29,30 +29,32 @@ public class Calculator {
 		System.out.print("Press '1' to add two numbers\nPress '2' to subtract two numbers\nPress '3' to multiply two numbers\nPress '4' to divide two numbers\n\nYour choice: ");
 
 		choice = sc.nextInt();
-		if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
-			System.out.println("Invalid choice");
 
-		else {
+		while(choice != 1 && choice != 2 && choice != 3 && choice != 4) {
 
-			System.out.print("Enter the numbers: ");
-			num1 = sc.nextInt();
-			num2 = sc.nextInt();
-
-			if (choice == 1)
-				System.out.println("Sum is: " + add(num1, num2));
-
-			else if (choice == 2)
-				System.out.println("Difference is: " + subtract(num1, num2));
-
-			else if (choice == 3)
-				System.out.println("Product is: " + multiply(num1, num2));
-
-			else if (choice == 4)
-				System.out.println("Result of division is: " + divide(num1, num2));
-
-			else
-				System.out.println("Invalid Input");
+			System.out.print("Invalid choice, try again\nYour choice: ");
+			choice = sc.nextInt();
 		}
+
+
+		System.out.print("Enter the numbers: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+
+		if (choice == 1)
+			System.out.println("Sum is: " + add(num1, num2));
+
+		else if (choice == 2)
+			System.out.println("Difference is: " + subtract(num1, num2));
+
+		else if (choice == 3)
+			System.out.println("Product is: " + multiply(num1, num2));
+
+		else if (choice == 4)
+			System.out.println("Result of division is: " + divide(num1, num2));
+
+		else
+			System.out.println("Invalid Input");
 
 	}
 
